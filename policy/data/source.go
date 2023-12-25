00eff2417074fb8afa993b555513fbabb823847e
Paul Norton <pnorton@duckblade.com>
2023-12-25 15:09:43 -0500

    add GqlContext helper

--- policy/data/source.go
+++ policy/data/source.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"context"
+	"github.com/atomist-skills/go-skill"
 )
 
 type QueryResponse struct {
@@ -11,4 +12,11 @@ type QueryResponse struct {
 
 type DataSource interface {
 	Query(ctx context.Context, queryName string, query string, variables map[string]interface{}) (*QueryResponse, error)
+}
+
+func GqlContext(req skill.RequestContext) map[string]interface{} {
+	return map[string]interface{}{
+		"teamId":       req.Event.WorkspaceId,
+		"organization": req.Event.Organization,
+	}
 }
