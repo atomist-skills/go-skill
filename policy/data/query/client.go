49c5f4bf4e53601858bd3fa21128d02cebdfc49e
Chris Ainsworth-Patrick <chris.ainsworth-patrick@docker.com>
2024-09-05 15:53:37 +0100

    some policies need gqlcontext

--- policy/data/query/client.go
+++ policy/data/query/client.go
@@ -2,6 +2,8 @@ package query
 
 import (
 	"context"
+
+	"github.com/atomist-skills/go-skill/policy/goals"
 )
 
 type QueryResponse struct {
@@ -10,4 +12,18 @@ type QueryResponse struct {
 
 type QueryClient interface {
 	Query(ctx context.Context, queryName string, query string, variables map[string]interface{}, output interface{}) (*QueryResponse, error)
+}
+
+func GqlContext(ctx goals.GoalEvaluationContext) map[string]interface{} {
+	result := map[string]interface{}{}
+
+	if ctx.TeamId != "" {
+		result["teamId"] = ctx.TeamId
+	}
+
+	if ctx.Organization != "" {
+		result["organization"] = ctx.Organization
+	}
+
+	return result
 }
